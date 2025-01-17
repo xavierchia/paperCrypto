6f9d44192de2b8a9c67458c26d711dc956bdf71e
xavier chia <xavier.cpy@gmail.com>
2025-01-17 14:58:25 +0800

    first commit

---
+++ PaperCrypto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaperCrypto
+//
+//  Created by xavier chia on 17/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PaperCryptoUITests/PaperCryptoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PaperCryptoUITestsLaunchTests.swift
+//  PaperCryptoUITests
+//
+//  Created by xavier chia on 17/1/25.
+//
+
+import XCTest
+
+final class PaperCryptoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
